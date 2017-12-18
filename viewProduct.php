fb7f54dad16b18d66c57124195f13d076afcec0d
Ankit <sharma.b.ankit92@gmail.com>
2017-12-18 03:11:55 -0500

    view product page layout done.

--- viewProduct.php
+++ viewProduct.php
@@ -12,4 +12,81 @@ require_once ("config.php");
 <?php
 require_once("navigationMenu.php");
 ?>
-</body>
+<!-- Product section -->
+<section id="services" class="services service-section">
+    <div class="container ">
+        <div class="section-header">
+            <h2 style="color: white" class="wow fadeInDown animated"><br>Product name</h2>
+            <hr>
+            <div class="row">
+                <div class="col-md-5">
+                    <img class="product-image" src="images/product_images/V-moda/1.jpg">
+                </div>
+                <div class="col-md-7">
+                    <h4 class="product-detail">V-moda Cross fade </h4>
+                    <br>
+                    <h4>Item Details  ->  Bluetooth over the ear headphone </h4>
+                    <h4 class="viewproduct-price">Price: 199 </h4>
+                    <hr class="viewproduct-divider">
+                    <br>
+                    <h4> QTY: <input type="number" class="qty-btn"></h4>
+                    <button type="submit" id="submit" name="send" class="btn-primary btn-kart">Add To Cart</button>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-5">
+                    <img class="product-image" src="images/product_images/V-moda/2.jpg">
+                </div>
+                <div class="col-md-7">
+                    <h4 class="product-detail">V-moda Cross fade </h4>
+                    <br>
+                    <h4>Item Details  ->  Bluetooth over the ear headphone </h4>
+                    <h4 class="viewproduct-price">Price: 199 </h4>
+                    <hr class="viewproduct-divider">
+                    <br>
+                    <h4> QTY: <input type="number" class="qty-btn"></h4>
+                    <button type="submit" id="submit" name="send" class="btn-primary btn-kart">Add To Cart</button>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-5">
+                    <img class="product-image" src="images/product_images/Marshall/3.png">
+                </div>
+                <div class="col-md-7">
+                    <h4 class="product-detail">V-moda Cross fade </h4>
+                    <br>
+                    <h4>Item Details  ->  Bluetooth over the ear headphone </h4>
+                    <h4 class="viewproduct-price">Price: 199 </h4>
+                    <hr class="viewproduct-divider">
+                    <br>
+                    <h4> QTY: <input type="number" class="qty-btn"></h4>
+                    <button type="submit" id="submit" name="send" class="btn-primary btn-kart">Add To Cart</button>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-5">
+                    <img class="product-image" src="images/product_images/Sony/3.jpg">
+                </div>
+                <div class="col-md-7">
+                    <h4 class="product-detail">V-moda Cross fade </h4>
+                    <br>
+                    <h4>Item Details  ->  Bluetooth over the ear headphone </h4>
+                    <h4 class="viewproduct-price">Price: 199 </h4>
+                    <hr class="viewproduct-divider">
+                    <br>
+                    <h4> QTY: <input type="number" class="qty-btn"></h4>
+                    <button type="submit" id="submit" name="send" class="btn-primary btn-kart">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+</section>
+<!-- Product section -->
+<?php
+require_once ("footer.php");
+?>
